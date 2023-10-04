4f7520955449485752244ed30c86deb64e0c1dac
JoaoAntonioSS <106708105+JoaoAntonioSS@users.noreply.github.com>
2023-10-04 16:44:08 -0300

    Mais uma parte do código e alguns comentários
    Irei comentar o resto do código mais tarde

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ Esse projeto foi baseado na criação de outros desenvolvedores
 https://github.com/omariosouto/flappy-bird-devsoutinho/blob/master/jogo.js
 https://www.youtube.com/playlist?list=PLTcmLKdIkOWmeNferJ292VYKBXydGeDej
 */
+let frames = 0;
+const som_HIT = new Audio();
+som_HIT.src = `./efeitos/hit.wav`
 const sprites = new Image()//Cria um espaço de imagem
 sprites.src = './sprites.png'//Adiciona a imagem 'sprites.png a esse espaço'
 const canvas = document.querySelector('canvas')//seleciona o objeto html 'canvas'
@@ -50,13 +53,23 @@ const planoDeFundo = {//registro para o plano de fundo dos prediozinhos
         )  //aplicado novamente somando planoDeFundo.x + planoDeFundo.largura pois somente com o primeiro drawImage a largura do canvas não é totalmente preenchida    
      }
 }
+function criachao(){
 const chao = {//registro para o chão do cenário
-     spriteX : 0,
+     spriteX: 0,
      spriteY: 610,
      largura: 224,
      altura: 112,
      x:0,
      y:canvas.height -112,//mesmo raciocínio das linhas 12, 14, 16
+    atualiza(){
+        const MovimentoDoChao = 1;
+        const RepeteEm = chao.largura/2;
+        const movimentacao = chao.x - MovimentoDoChao;
+        console.log(`chao.x`,chao.x);
+        console.log(`RepeteEm`,RepeteEm);
+        console.log(`movimentacao`,movimentacao % RepeteEm)
+        chao.x=movimentacao % RepeteEm;
+     },
      desenha(){
         contexto.drawImage(
             sprites,
@@ -74,38 +87,96 @@ const chao = {//registro para o chão do cenário
         )//mesmo raciocíno da linha 50
      }
 }
+    return chao
+}
+function fazColisao(passaro,chao){
+    const passaroY = passaro.y + passaro.altura;
+    const chaoY = chao.y;
+
+    if(passaroY >= chaoY){
+        return true;
+    }
+    return false;
+}
+function criapassaro(){
 const passaro = {
-     spriteX : 0,
+     spriteX: 0,
      spriteY: 0,
      largura: 33,
      altura: 24,
      x:10,
      y:50,//mesmo raciocínio das linhas 12, 14, 16
+    pulo: 4.6,
+    pula(){
+           console.log(`devo pular`)
+           console.log(`antes`,passaro.velocidade)
+               passaro.velocidade = -passaro.pulo;
+        },//faz o vôo do pássaro ocorrer, ao relacionar o salto e a velocidade de forma inversa
      velocidade: 0,//velocidade inicial sendo zero, pois antes de o jogo começar o pássaro fica parado
      gravidade: 0.12,//gravidade que se tornou mais realista até o momento
-     atualiza(){//função para atualizar a posição do pássaro no fps determinado pela requestAnimationFrame
-        passaro.velocidade = passaro.velocidade + passaro.gravidade//A cada atualização de quadros a velocidade é somada com a gravidade
-        passaro.y += passaro.velocidade//A cada atualização de quadros, a coordenada y do ponto de referência para desenhar o recorte de sprites.png no canvas aumenta, fazendo o pássaro cair realisticamente 
+     atualiza(){//função para atualizar a posição do pássaro no fps determinado pela requestAnimationFrame fazendo o pássaro cair realisticamente
+    if (fazColisao(passaro,globais.chao)){
+                   console.log(`fez colisão`)
+                   som_HIT.play();
+                   setTimeout(() =>< {
+
+                   },1000 );
+                   mudaParaTela(telas.inicio);
+                   return;
+           }
+           passaro.velocidade = passaro.velocidade + passaro.gravidade//A cada atualização de quadros a velocidade é somada com a gravidade
+           passaro.y += passaro.velocidade//A cada atualização de quadros, a coordenada y do ponto de referência para desenhar o recorte de sprites.png no canvas aumenta, fazendo o pássaro cair realisticamente 
+        },
+        movimentos: [
+                {spriteX:0, spriteY:0},//recorte no sprite com o pássaro com asa pra cima
+                {spriteX:0, spriteY:26},// pássaro com asa no meio
+                {spriteX:0, spriteY:52},//pássaro com asa pra baixo
+                {spriteX:0, spriteY:26},// pássaro com asa no meio
+        ],
+        FrameAtual: 0,
+        AtualizaoFrameAtual(){
+            const IntervalodeFrames = 10;
+            const passouoIntervalo = frames % IntervalodeFrames === 0;
+            console.log(`passouointervalo`, passouoIntervalo)
+            if (passouoIntervalo) {
+            const BasedoIncremento = 1;
+            const incremento = BasedoIncremento + passaro.FrameAtual;
+            const BaseRepeticao = passaro.movimentos.length;
+            passaro.FrameAtual = incremento % BaseRepeticao;
+            }
+        },
      },
      desenha(){
+         passaro.AtualizaoFrameAtual();
+            const {spriteX,spriteY} = passaro.movimentos[passaro.FrameAtual];
         contexto.drawImage(
             sprites,
-            passaro.spriteX, passaro.spriteY,
-            passaro.largura,passaro.altura,
-            passaro.x,passaro.y,
-            passaro.largura,passaro.altura
+               spriteX,spriteY,
+               passaro.largura,passaro.altura,
+               passaro.x,passaro.y,
+               passaro.largura,passaro.altura
         )
      }//mesmo raciocínio da linha 25
 }
+    return passaro;
+}
+const globais = {};
 let telaAtiva = {}// variável, pois como a tela ativa vai variar não têm como ser um valor constante
 const mudaParaTela = (novaTela) =>{
     telaAtiva = novaTela
+    if (telaAtiva.inicializa){
+        inicializa();
+    }
 }   // função que transforma o registro vazio (ou com o registro de uma tela específica) telaAtiva no registro da tela desejada
 const telas = {
     inicio:{
+        inicializa(){
+           globais.passaro = criapassaro();
+           globais.chao = criachao();
+        },
         desenha(){
             planoDeFundo.desenha()
-            chao.desenha()
+            globais.chao.desenha()
             passaro.desenha()
             mensagemGetReady.desenha()
         },// na tela do início, será desenhado o plano de fundo do céu azul e prediozinhos, o chão, o pássaro e a mensagem de GetReady
@@ -113,22 +184,29 @@ const telas = {
             mudaParaTela(telas.jogo) // com o evento do click do mouse, será disparada a função mudaParaTela com o argumento telas.jogo, ou seja, vai trocar da tela de início para tela de Jogo 
         },
         atualiza(){
+            globais.chao.desenha;
         } // no momento, não há nada para ficar movimentando na tela inicial do jogo
     },
     jogo:{
         desenha(){
             planoDeFundo.desenha()
-            chao.desenha()
-            passaro.desenha()
-        }, //mesmo raciocínio da linha 111, mas sem a mensagem de GetReady, já que o jogo já começou
+            globais.chao.desenha()
+            globais.passaro.desenha()
+        },
+         //mesmo raciocínio da linha 111, mas sem a mensagem de GetReady, já que o jogo já começou
+         click () {
+            globais.passaro.pula
+         },
+         //atualiza o pássaro ao clicar na tela
         atualiza(){
-            passaro.atualiza()// na tela de jogo o pássaro vai ficar se movimentando, caindo
+            globais.passaro.atualiza()// na tela de jogo o pássaro vai ficar se movimentando, caindo
         }
     }
 }
 const loop = ()=>{// função que vai ser chamada toda hora, em loop
     telaAtiva.desenha()//desenha o que tiver que desenhar da tela ativa no momento
     telaAtiva.atualiza()// atualiza o que tiver que atualizar da tela ativa no momento
+    frames = frames + 1;
     requestAnimationFrame(loop) // função que vai ativar o fps, quadros por segundo, que precisa chamar a função loop toda hora, ou seja, desenhar e atualizar toda hora. Como a requestAnimationFrame junto com a drawImage dó funciona com esse esquema de laço, não dá pra trocar por algo mais funcional.
 }
 window.addEventListener('click', ()=>{ // observa os eventos na janela
